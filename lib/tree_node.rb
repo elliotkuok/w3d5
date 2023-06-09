d80451b2567bedaadb50e91253440407535600c0
Elliot Kuok <elliotckuok@gmail.com>
2023-06-09 12:16:24 -0700

    Finish parent assignment method

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
-  
+    attr_reader :children, :parent, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    @parent.children.delete(self) if @parent 
+    @parent = new_parent
+    
+    if new_parent
+        @parent.children << self
+    end
+  end
 end
