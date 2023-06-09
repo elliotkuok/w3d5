c56553ac1a65ff127cde45e41a0a410283574cfc
Elliot Kuok <elliotckuok@gmail.com>
2023-06-09 15:30:03 -0700

    fixing valid moves and new move positions

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -6,18 +6,43 @@ class KnightPathFinder
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
     @move_tree = self.build_move_tree
+    @considered_positions = [pos]
   end
 
   def build_move_tree
     self.root_node
   end
 
+  def self.valid_moves(pos)
+    valid_moves = []
+    possible_moves = [
+      [1,2],[1,-2],[2,1],[-2,1],
+      [-1,2],[-1,-2],[2,-1],[-2,-1]
+    ]
+
+    possible_moves.each do |move| 
+      row = pos[0] + move[0]
+      col = pos[1] + move[1]
+      if row > 0 && col > 0
+        valid_moves << [row,col]
+      end
+    end
+    valid_moves
+  end
+
+  def new_move_positions(pos)
+    valid_moves = KnightPathFinder.valid_moves(pos)
+    new_moves = valid_moves.select {|move| !@considered_positions.include?(move)}
+    new_moves.each {|move| @considered_positions << move}
+  end
+
+
 
 end
 
-kp = KnightPathFinder.new([0,0])
-p kp.root_node
-p kp.move_tree
+kp = KnightPathFinder.new([4,4])
+p kp.new_move_positions([4,4])
+
 
 
 
