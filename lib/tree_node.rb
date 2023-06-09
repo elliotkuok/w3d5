b61467364e903c35d1c087cad261cbde3f645b08
Julio Uribe <uribejulioc@gmail.com>
2023-06-09 12:28:54 -0700

    Add 'add' and 'remove' methods to treenode class'

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -8,11 +8,28 @@ class PolyTreeNode
   end
 
   def parent=(new_parent)
-    @parent.children.delete(self) if @parent 
+    @parent.children.delete(self) if @parent
     @parent = new_parent
-    
+
     if new_parent
         @parent.children << self
     end
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    @children.include?(child_node) ? child_node.parent = nil : raise
+  end
 end
+
+
+"""
+    a
+   / \
+  b   c
+ /
+d
+"""
