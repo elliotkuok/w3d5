454d2d7deb91626b46b0c906cc96700b10fd0b3b
Julio Uribe <uribejulioc@gmail.com>
2023-06-09 17:08:40 -0700

    Finish part 1 of Knights Travails

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,16 +1,30 @@
 require_relative("treenode")
+require("byebug")
 
 class KnightPathFinder
-  attr_reader :root_node, :move_tree
+  attr_reader :root_node, :move_tree, :considered_positions
 
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
-    @move_tree = self.build_move_tree
     @considered_positions = [pos]
+    @move_tree = self.build_move_tree
   end
 
   def build_move_tree
-    self.root_node
+    q = Queue.new
+    q.enq(@root_node)
+
+    until q.empty?
+      current = q.deq
+      # Create new nodes with possible move positions.
+      new_pos = new_move_positions(current.value)
+      new_pos.each do |pos|
+        new_node = PolyTreeNode.new(pos)
+        current.add_child(new_node)
+        new_node.parent = current
+        q << new_node
+      end
+    end
   end
 
   def self.valid_moves(pos)
@@ -20,10 +34,11 @@ class KnightPathFinder
       [-1,2],[-1,-2],[2,-1],[-2,-1]
     ]
 
-    possible_moves.each do |move| 
+    possible_moves.each do |move|
       row = pos[0] + move[0]
       col = pos[1] + move[1]
-      if row > 0 && col > 0
+      # Make sure move is on 8x8 board.
+      if (row > 0 && col > 0) && (row <=7 && col <= 7)
         valid_moves << [row,col]
       end
     end
@@ -32,6 +47,7 @@ class KnightPathFinder
 
   def new_move_positions(pos)
     valid_moves = KnightPathFinder.valid_moves(pos)
+    # Filter out moves that have not yet been considered.
     new_moves = valid_moves.select {|move| !@considered_positions.include?(move)}
     new_moves.each {|move| @considered_positions << move}
   end
@@ -40,16 +56,17 @@ class KnightPathFinder
 
 end
 
-kp = KnightPathFinder.new([4,4])
-p kp.new_move_positions([4,4])
-
+kp = KnightPathFinder.new([0,0])
+# p kp.new_move_positions([4,4])
+# root = kp.root_node
+# root.children[1].children[0].children[0].value
 
 
 
 """
-    knight start (type of move, position)
-      /     \
-    move 1  move1
-  /
-move 2
+            knight start [0,0]
+              /     \
+            b       c
+          /   \     / \
+        m1    m2   m1  m2
 """
